60430a5ded50434a35c8eb3a6f0a2b557279335a
Kelsey Jacobsen <kelseyingrid@gmail.com>
2019-07-14 22:43:03 -0700

    July 14 nav bar and other assessment revisons

--- main.js
+++ main.js
@@ -15,3 +15,26 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+// $(window).scroll(function() {
+// // 100 = The point you would like to fade the nav in.
+//
+// 	if ($(window).scrollTop() > 100 ){
+//
+//  		$('.bg').addClass('show');
+//
+//   } else {
+//
+//     $('.bg').removeClass('show');
+//
+//  	};
+// });
+//
+// $('.scroll').on('click', function(e){
+// 		e.preventDefault()
+// 
+//   $('html, body').animate({
+//       scrollTop : $(this.hash).offset().top
+//     }, 1500);
+// });
