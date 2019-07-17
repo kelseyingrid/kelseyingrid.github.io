a176aeb72e67f50ddbafbafc3c8ae460ff1d6123
Kelsey Jacobsen <kelseyingrid@gmail.com>
2019-07-16 17:14:30 -0700

    July 16 more navbar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector("#sub");
+const nav = document.querySelector(".process-steps");
 const topOfNav = nav.offsetTop;
 const main = document.getElementById("main");
 
@@ -13,50 +13,3 @@ function fixNav() {
 }
 
 window.addEventListener("scroll", fixNav)
-
-
-
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-//
-// // Get the header
-// var header = document.getElementById("myHeader");
-// var container = document.getElementById("myContainer");
-//
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
-//
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//     document.getElementById("myContainer").style.paddingTop = "8rem";
-//   } else {
-//     header.classList.remove("sticky");
-//     container.attributeList.remove("paddingTop");
-//   }
-// }
-
-// document.getElementsByTagName("sticky").style.paddingTop = "100rem";
-
-// $(window).scroll(function() {
-// // 100 = The point you would like to fade the nav in.
-//
-// 	if ($(window).scrollTop() > 100 ){
-//
-//  		$('.bg').addClass('shows
-//
-//   } else {
-//
-//     $('.bg').removeClass('show');
-//
-//  	};
-// });
-//
-// $('.scroll').on('click', function(e){
-// 		e.preventDefault()
-//
-//   $('html, body').animate({
-//       scrollTop : $(this.hash).offset().top
-//     }, 1500);
-// });
