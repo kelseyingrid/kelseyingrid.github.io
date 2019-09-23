a0b966e72c9a6ee1ebee412eb7d3a43cdf7531e2
Kelsey Jacobsen <kelseyingrid@gmail.com>
2019-09-22 18:28:59 -0700

    Changed 80 to 87

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector(".process-steps-flex");
-const topOfNav = nav.offsetTop - 80;
+const topOfNav = nav.offsetTop - 87;
 const main = document.getElementById("main");
 
 function fixNav() {
